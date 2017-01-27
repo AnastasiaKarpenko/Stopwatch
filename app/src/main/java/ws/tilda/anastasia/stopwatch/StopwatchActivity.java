c50a5358918e5c2177c4adefe0416c303b967b83
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-01-27 10:56:35 +0200

    slighly changed layout

--- app/src/main/java/ws/tilda/anastasia/stopwatch/StopwatchActivity.java
+++ app/src/main/java/ws/tilda/anastasia/stopwatch/StopwatchActivity.java
@@ -51,7 +51,7 @@ public class StopwatchActivity extends AppCompatActivity {
                 int minutes = (seconds%3600)/60;
                 int secs = seconds%60;
 
-                String time = String.format("%d: %02d: %02d", hours, minutes, secs);
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
                 timeView.setText(time);
                 if(running) {
                     seconds++;
